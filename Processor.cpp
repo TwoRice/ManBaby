a523dff7a82b95552ec63af51522b1d8fc0da34c
TwoRice <2rimagowan@gmail.com>
2015-11-20 22:13:07 +0000

    Fixed Errors

--- Processor.cpp
+++ Processor.cpp
@@ -9,84 +9,63 @@ Processor::Processor(){
 
 	for (int i = 0; i < 32; i++)
 	{
-		bool accumulator[i] = 0;
-		bool ci[i] = 0;
-		bool pi[i] = 0;
+		accumulator[i] = 0;
+		ci[i] = 0;
+		pi[i] = 0;
 	}
 
 }
 
-/**
-*Method which resets each element of ci[] to 0
-*
-*/
 void Processor::resetCI(){
 
 	for (int i = 0; i < 32; i++)
 	{
-		bool ci[i] = 0;
+		ci[i] = 0;
 	}
 
 }
 
-/**
-*Method which converts binary array into decimal integer
-*
-*param a: bool binary[] - binary array for conversion
-*return : int decimal	- binary array converted to integer
-*/
-int Processor::convertBinToDec(bool binary[]){
+int Processor::convertBinToDec(bool binary[], int length){
 
-int decimal = 0;
-int length = binary.length();
+	int decimal = 0;
 
-	for (int i = length - 1; i >= 0 i--)
-	{
-		decimal += (int(binary[i]) -48)*pow(2,i);
-	}
+		for (int i = length - 1; i >= 0; i--)
+		{
+			decimal += (int(binary[i]))*pow(2,i);
+		}
 
-cout << "Conversion of Binary to Decimal complete" << endl;
-cout << decimal << endl;
-return decimal;
+	cout << "Conversion of Binary to Decimal complete" << endl;
+	cout << decimal << endl;
+	return decimal;
 
 }
 
-/**
-*Method which converts decimal integer into binary array
-*
-*param a: int decimal - decimal number for conversion
-*return : array[]	  - decimal number converted to binary
-*/
 bool* Processor::convertDecToBin(int decimal){
 
-bool array[32]; 
-int index = 0;
+	bool array[32]; 
+	int index = 0;
 
-	for (int i = 0; i < 32; i++)
-	{
-		array[i] = 0;
-	}
+		for (int i = 0; i < 32; i++)
+		{
+			array[i] = 0;
+		}
 
-	while(decimal !=0){
-			
-			array[index] = decimal%2;
-			decimal/=2;
-			index++;
-	}
-	
-return array;
+		while(decimal !=0){
+				
+				array[index] = decimal%2;
+				decimal/=2;
+				index++;
+		}
+		
+	return array;
 }
 
-/**
-*Method which converts ci[] into decimal, increments the result
-*and finally, converts the decimal integer back into ci[] 
-*
-*/
-void increment(){
+void Processor::increment(){
 
-		int decimal = convertBinToDec(ci);
-		decimal++;
-		ci = convertDecToBin(decimal);
+	int decimal = convertBinToDec(ci,32);
+	decimal++;
+	*ci = convertDecToBin(decimal);
+	
 }
 
 int Processor::fetchOperand(){
@@ -99,7 +78,7 @@ int Processor::fetchOperand(){
 
 	}
 
-	int memLoc = converToDec(operand);
+	int memLoc = convertBinToDec(operand,5);
 
 	return memLoc;
 
