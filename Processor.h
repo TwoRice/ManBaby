b3558632b094cec0b68df12d42897cfe2421aa52
TwoRice <2rimagowan@gmail.com>
2015-11-21 23:13:18 +0000

    Update Processor.h

--- Processor.h
+++ Processor.h
@@ -9,15 +9,12 @@ class Processor{
 	public : 	
 
 		Processor();
-		void setAccumulator(bool accumulator[32]);
-		bool* getAccumulator();
-		void setCI(bool memoryaddress[32]);
 		bool* getCI();
 		void setPI(bool instruction[32]);
 		void resetCI();
 		int convertBinToDec(bool binary[], int length);
 		bool* convertDecToBin(int decimal);
-		int twosComp(bool operand[32]);
+		int negate(bool operand[32]);
 		void increment();
 		int getOp(int start, int end);
 
