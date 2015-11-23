fac09bb32955609a8cdce247f9d5d719bd7be131
TwoRice <2rimagowan@gmail.com>
2015-11-23 01:52:22 +0000

    Finished

--- Store.h
+++ Store.h
@@ -1,7 +1,17 @@
+/**
+*Class which holds the nessessary data structures and logic to simulate the Manchester Baby's Store.
+*(memory)
+*
+*Authors : Rory Magowan, Daniel Bereton, Daniel Kinnaird
+*Version : 1.0 22nd November 2015
+*
+*/
+
 class Store{
 
 	private :
 
+		//2D boolean array for the memory
 		bool memory[32][32];
 
 	public : 
@@ -9,7 +19,6 @@ class Store{
 		Store();
 		bool* readMemory(int location);
 		void writeMemory(int location, bool data[32]);
-		void loadProgram(string fileName);
+		bool loadProgram(string fileName);
 
 };
-
