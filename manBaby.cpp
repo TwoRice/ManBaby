2fa347718aa58aaed965ddede490318f906e4162
TwoRice <2rimagowan@gmail.com>
2015-11-23 01:51:08 +0000

    Finished

--- manBaby.cpp
+++ manBaby.cpp
@@ -1,3 +1,11 @@
+/**
+*Program to simulate the hardware of the Manchester Baby which is able to take txt files of machine
+*code and execute them
+* 
+*Authors : Rory Magowan, Daniel Bereton, Daniel Kinnaird
+*Version : 1.0 22nd November 2015
+*
+*/
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -7,43 +15,169 @@
 using namespace std;
 
 
+/**
+*Class which for the Manchester Baby which contains the processor and the store and the functions
+*they require to perform the fetch execute cycle
+*
+*/
 class ManBaby{
 
 	private :
 
+		//Object for the processor
 		Processor theProcessor;
+		//Object for the store (memory)
 		Store theStore;
 
 	public : 
 		
-		void loadProgramToMemory();	
+		void printCI();
+		void printPI();
+		void printAcc();
+		void printStore();
+		void printStoreLine(int operand);
+		bool loadProgramToMemory();	
 		void jmp(int operand);
 		void jrp(int operand);
 		void ldn(int operand);
 		void sto(int operand);
 		void sub(int operand);
 		void cmp();
-		void stp();
 		void fetch();
-		void decode();
-		void execute(int opcode, int operand);
+		bool decode();
+		bool execute(int opcode, int operand);
+		void run();
 
 
 };
 
-void ManBaby::loadProgramToMemory(){
+/**
+*Method to print the contents of the CI (Control Instruction)
+*
+*/
+void ManBaby::printCI(){
+
+	cout << "CI : ";
+	for(int i = 0; i < 32; i++){
+
+		cout << theProcessor.getCI()[i];
+
+	}
+	cout << endl;
+
+
+}
+
+/**
+*Method to print the contents of the PI (Present Instruction)
+*
+*/
+void ManBaby::printPI(){
+
+	cout << "PI : ";
+	for(int i = 0; i < 32; i++){
+
+		cout << theProcessor.getPI()[i];
+
+	}
+	cout << endl;
+
+
+}
+
+/**
+*Method to print the contents of the Accumulator
+*
+*/
+void ManBaby::printAcc(){
+
+	cout << "Accumulator : ";
+	for(int i = 0; i < 32; i++){
+
+		cout << theProcessor.getAccumulator()[i];
+
+	}
+	cout << endl;
+
+
+}
+
+/**
+*Method to print the contents of the store (memory)
+*
+*/
+void ManBaby::printStore(){
+
+	cout << "Store : " << endl;
+	
+	for(int j = 0; j < 32; j++){
+
+		for(int i = 0; i < 32; i++){
+
+			cout << theStore.readMemory(j)[i];
+
+		}
+		cout << endl;
+
+	}
+
+}
+
+/**
+*Method to print the contents of the a given line of memory in the store (memory)
+*
+*param : operand - the memory location for the line of memory to be printed
+*/
+void ManBaby::printStoreLine(int operand){
+
+	cout << "Value at Store : ";
+	for(int i = 0; i < 32; i++){
+
+		cout << theStore.readMemory(operand)[i];
+
+	} 
+	cout << endl;
+
+}
+
+/**
+*Method to take the file name of for the txt file containing the machine code to be executed and
+*then load the contents of said file into the stoe (memory)
+*
+*return : whether or not the file entered by the user exists or not
+*/
+bool ManBaby::loadProgramToMemory(){
 
 	string fileName;
+	bool exists;
+
 	cout << "Enter Filename (Without Extension): " << endl;
 	getline(cin, fileName);
+	//adds the files extension to the file name
 	fileName.append(".txt");
-	theStore.loadProgram(fileName);
+	exists = theStore.loadProgram(fileName);
+	if(exists){
+
+		theProcessor.resetCI();
+		theProcessor.resetAccumulator();
+		printStore();
+		cout << endl;
+		return true;
+
+	}
+
+	else{
+
+		return false;
+
+	}
 
 
 }
 
 /**
 *Method which jumps to the instruction at the address obtained from the specific memory address
+*by setting the CI to the value stored at said address
 *
 *param a: int operand - integer to be used for operation
 */
@@ -68,7 +202,7 @@ void ManBaby::jrp(int operand){
 }
 
 /**
-*Method which takes the number from the specified memory address _!_
+*Method which takes the number from the specified memory address and puts it into the accumulator
 *
 *param a: int operand - integer to be used for operation
 */
@@ -79,7 +213,7 @@ void ManBaby::ldn(int operand){
 }
 
 /**
-*Method which stores the number in the accumulator to the specified memory address _!_
+*Method which stores the number in the accumulator at the specified memory address
 *
 *param a: int operand - integer to be used for operation
 */
@@ -90,67 +224,184 @@ void ManBaby::sto(int operand){
 }
 
 /**
-*Method which subtracts the number at the specified memory address _!_ from the value in the accumulator
+*Method which subtracts the number at the specified memory address from the value in the accumulator
 *and then stores the result in the accumulator
 * 
 *param a: int operand - integer to be used for operation
 */
-void sub(int operand){
+void ManBaby::sub(int operand){
 
-	int accuDecimal = theProcessor.convertBinToDec(accumulator, 32);
-	int memStore = theProcessor.convertBinToDec(theStore.readMemory(operand)); 
+	int accuDecimal = theProcessor.convertBinToDec(theProcessor.getAccumulator(), 32);
+	int memStore = theProcessor.convertBinToDec(theStore.readMemory(operand), 32); 
 	accuDecimal -= memStore;
-	accumulator = theProcessor.setAccumulator(theProcessor.convertDecToBin(accuDecimal));
+	theProcessor.setAccumulator(theProcessor.convertDecToBin(accuDecimal));
 }
 
 /**
 *Method which skips the next instruction if the accumulator contains a negative number
 *
 */
-void cmp(){
+void ManBaby::cmp(){
 
-	int accuDecimal = theProcessor.convertBinToDec(accumulator, 32);	
+	int accuDecimal = theProcessor.convertBinToDec(theProcessor.getAccumulator(), 32);	
 
 			if(accuDecimal < 0){
 				theProcessor.increment();
 			}
+
 }
+
 
 /**
-*Method which stops the program
+*Method which fetches the present instruction from the store (memory)
 *
-*param a: int operand - integer to be used for operation
 */
-void ManBaby::stp(int operand){
-
-	
-
-}
-
 void ManBaby::fetch(){
 
 	int memoryAddress;
 
-	memoryAddress = theProcessor.convertBinToDec(theProcessor.getCI(),32);
+	printCI();
 	theProcessor.increment();
+	memoryAddress = theProcessor.convertBinToDec(theProcessor.getCI(),32);
+	printCI();
 	theProcessor.setPI(theStore.readMemory(memoryAddress));
+	printPI();
 
 }
 
-void ManBaby::decode(){
+/**
+*Method which extratcs the operand and opcode from the presnet instruction and uses them for the
+*execute method
+*
+*return - whether or not the program has finished
+*/
+bool ManBaby::decode(){
 
 	int operand;
 	int opcode;
-	bool* binNum;
 	int num;
 
+	cout << "Operand : ";
+	//Gets the operand as the first 5 values in the present instruction
 	operand = theProcessor.getOp(0, 4);
+	cout << "Opcode : ";
+	//Gets the opcode as the values from position 13 to position 15 in the present instrcution
 	opcode = theProcessor.getOp(13, 15);
-	
-	binNum = theStore.readMemory(operand);
-	operand = theProcessor.convertBinToDec(binNum, 32);
+	cout << endl;
 
-	//execute(opcode, operand);
+	execute(opcode, operand);
+
+}
+
+/**
+*Method which uses ths decoded instruction to execute the instruction
+*
+*return - whether or not the program has finished
+*/
+bool ManBaby::execute(int opcode, int operand){
+
+	switch(opcode){
+
+		case 0 :
+
+			cout << "JMP" << endl;
+			jmp(operand);
+			printStoreLine(operand);
+			printCI();
+			cout << endl;
+			break;
+
+		case 1 : 
+
+			cout << "JRP" << endl;
+			printCI();
+			jrp(operand);
+			printStoreLine(operand);
+			printCI();
+			cout << endl;
+			break;
+
+		case 2 : 
+
+			cout << "LDN" << endl;
+			ldn(operand);
+			printStoreLine(operand);	
+			printAcc();	
+			cout << endl;
+			break;	
+
+		case 3 : 
+		
+			cout << "STO" << endl;
+			sto(operand);
+			printAcc();	
+			printStoreLine(operand);	
+			cout << endl;
+			break;
+
+		case 4 : 
+		
+			cout << "SUB" << endl;
+			printAcc();
+			printStoreLine(operand);
+			sub(operand);
+			printAcc();
+			cout << endl;
+			break;	
+			
+		case 5 : 
+		
+			cout << "SUB" << endl;
+			printAcc();
+			printStoreLine(operand);
+			sub(operand);
+			printAcc();
+			sub(operand);
+			cout << endl;
+			break;	
+			
+		case 6 : 
+		
+			cout << "CMP" << endl;
+			printAcc();
+			printCI();
+			cmp();
+			printCI();
+			break;	
+			
+		case 7 : 
+		
+			cout << "STP" << endl;
+			cout << "Program Terminated" << endl;
+			cout << endl;
+			return false;
+			break;
+
+		default : 
+		
+			cout << "Error in Machine Code, opcode does not exist" << endl;
+			break;										
+
+
+	}
+
+}
+
+/**
+*Method to load the program into memeory then run the fetch execute cycle until the stop instruction
+*is given
+*
+*/
+void ManBaby::run(){
+
+	bool run;
+	
+	if(loadProgramToMemory()){
+		do{
+			fetch();
+			run = decode();
+		}while(run);
+	}	
 
 }
 
@@ -186,7 +437,8 @@ int readInt(){
 
 
 /**
-* 
+* Displays a basic text menu to the user
+*
 */
 void displayMenu(){
 
@@ -194,6 +446,10 @@ void displayMenu(){
 	cout << "(2) Exit" << endl;
 }
 
+/**
+*Method to handle the user selection for the menu
+*
+*/
 void menu(){
 	
 	ManBaby theBaby;
@@ -207,22 +463,21 @@ void menu(){
 		option = readInt();
 
 		switch(option){
-			//Option 1: Calls the loadfile method -
-			//loadfile() .....................
+
 			case 1:
 				
-				theBaby.loadProgramToMemory();
-				theBaby.fetch();
-				theBaby.decode();
+				theBaby.run();
 
 				break;
 
 			case 2:
+
 				cout << "Exiting Program" << endl;
 
 				break;
 
 			default:
+
 				cout << "Invalid Option" << endl;
 
 				break;
@@ -235,7 +490,8 @@ void menu(){
 }
 
 /**
-* 
+*Main method where the program is run from 
+*
 */
 int main(){
 
