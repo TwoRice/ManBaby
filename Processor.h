32a489a77b34378cb414b6caf8645fee88f70cb7
TwoRice <2rimagowan@gmail.com>
2015-11-23 01:51:31 +0000

    Finished

--- Processor.h
+++ Processor.h
@@ -1,9 +1,20 @@
+/**
+*Class which holds the nessessary data structures and logic to simulate the Manchester Baby's processor.
+*
+*Authors : Rory Magowan, Daniel Bereton, Daniel Kinnaird
+*Version : 1.0 22nd November 2015
+*
+*/
+
 class Processor{
 
 	private : 
 
+		//Boolean Array for the Accumulator
 		bool accumulator[32];
+		//Boolean Array for the CI (Control Instruction)
 		bool ci[32];
+		//Boolean Array for the PI (Present Instruction)
 		bool pi[32];
 
 	public : 	
@@ -16,6 +27,7 @@ class Processor{
 		void setPI(bool instruction[32]);
 		bool* getPI();
 		void resetCI();
+		void resetAccumulator();
 		int convertBinToDec(bool binary[], int length);
 		bool* convertDecToBin(int decimal);
 		bool* negate(bool operand[32]);
